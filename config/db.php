b10012b5bc0cd78389f2f63d80b4a28d989e5f46
Lisseth Valentina Tirado Gomez <lissethvtg@gmail.com>
2026-06-20 16:43:12 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "21122406"; 
-$db   = "tienda_relojes";
+$host = "mysql-2b6cef58-lissethvtg-7b3f.aivencloud.com";
+$user = "avnadmin";
+$pass = "AVNS_oxWuaB71vtR8XESkjNh"; 
+$db   = "defaultdb";
+$port = 20544; 
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
+$conexion = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conexion) {
-    die("Error crítico de conexión: " . mysqli_connect_error());
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
