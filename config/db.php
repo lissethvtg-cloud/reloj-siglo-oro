891a95fac22e8d3b03d72971dd04a97ab177cd67
lisseth <valgom73@gmail.com>
2026-06-20 21:29:14 -0600

    Conexión simplificada sin SSL

--- config/db.php
+++ config/db.php
@@ -6,20 +6,10 @@ $pass = "AVNS_ihzjatSKhF24ksM343R";
 $db   = "defaultdb";
 $port = 20544;
 
-// Creamos la conexión de forma segura
-$conexion = mysqli_init();
+// Conexión estándar sin forzar SSL
+$conexion = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conexion) {
-    die("Error al inicializar MySQLi");
-}
-
-// Activamos SSL para que Aiven acepte la conexión desde Render
-mysqli_ssl_set($conexion, NULL, NULL, NULL, NULL, NULL);
-
-// Conectamos
-$conectar = mysqli_real_connect($conexion, $host, $user, $pass, $db, $port);
-
-if (!$conectar) {
-    die("Error crítico de conexión: " . mysqli_connect_error());
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
