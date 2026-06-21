a73b4c6c7345923939063a86d0304e1bc51f3fdc
lisseth <valgom73@gmail.com>
2026-06-20 21:39:30 -0600

    Corrección crítica de variables y host

--- config/db.php
+++ config/db.php
@@ -1,15 +1,16 @@
 <?php
-// Definimos las variables de conexión asegurándonos que coincidan exactamente
-$host = "mysql-2b6cef58-lissethvtg-7b3f.aivencloud.com";
-$user = "avnadmin";
-$pass = "AVNS_ihzjatSKhF24ksM343R"; 
-$db   = "defaultdb";
+// Configuración directa para evitar errores de variables no definidas
+$host = 'mysql-2b6cef58-lissethvtg-7b3f.aivencloud.com';
+$user = 'avnadmin';
+$pass = 'AVNS_ihzjatSKhF24ksM343R'; 
+$db   = 'defaultdb';
 $port = 20544;
 
-// Conexión estándar sin forzar SSL
+// Intentamos la conexión
 $conexion = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+    // Si falla, mostramos el error específico
+    die("Fallo de conexión: " . mysqli_connect_error());
 }
 ?>
