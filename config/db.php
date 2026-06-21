e1c84988e8df429895c42c276bc22ce36fc37ec3
lisseth <valgom73@gmail.com>
2026-06-20 18:49:32 -0600

    Conexión a Aiven configurada en la carpeta config

--- config/db.php
+++ config/db.php
@@ -3,11 +3,22 @@ $host = "mysql-2b6cef58-lissethvtg-7b3f.aivencloud.com";
 $user = "avnadmin";
 $pass = "AVNS_ihzjatSKhF24ksM343R"; 
 $db   = "defaultdb";
-$port = 20544; 
+$port = 20544;
 
-$conexion = mysqli_connect($host, $user, $password, $database, $port);
+// Inicializamos mysqli para poder configurar el SSL
+$conexion = mysqli_init();
 
 if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+    die("Error al inicializar MySQLi");
+}
+
+// Activamos el SSL obligatorio para Aiven (sin necesidad de un archivo físico)
+mysqli_ssl_set($conexion, NULL, NULL, NULL, NULL, NULL);
+
+// Realizamos la conexión real incluyendo el puerto de Aiven
+$conectar = mysqli_real_connect($conexion, $host, $user, $pass, $db, $port);
+
+if (!$conectar) {
+    die("Error crítico de conexión a la nube: " . mysqli_connect_error());
 }
 ?>
