da9c0b18c4a596527b3e86568a80f48aee78d4b1
lisseth <valgom73@gmail.com>
2026-06-21 13:04:54 -0600

    Conexión SSL optimizada para Aiven

--- config/db.php
+++ config/db.php
@@ -1,9 +1,16 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
-$port = getenv('DB_PORT');
+// Usaremos variables simples sin funciones extra que puedan fallar
+$host = 'mysql-2b6cef58-lissethvtg-7b3f.aivencloud.com';
+$user = 'avnadmin';
+$pass = 'AVNS_ihzjatSKhF24ksM343R'; 
+$db   = 'defaultdb';
+$port = 20544;
 
-$conexion = mysqli_connect($host, $user, $pass, $db, $port);
+$conexion = mysqli_init();
+// Esto ayuda a que no intente buscar certificados SSL que no tenemos configurados
+mysqli_options($conexion, MYSQLI_OPT_SSL_MODE, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+
+if (!mysqli_real_connect($conexion, $host, $user, $pass, $db, $port)) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
 ?>
